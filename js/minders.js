318b990b210e952e5245e7a42013a31f1f5619df
HadeerSayed <HadeerSayed986@gmail.com>
2020-09-21 20:15:29 +0200

    edit commit

---
+++ js/minders.js
@@ -0,0 +1,10 @@
+AOS.init();
+
+$(document).ready(function () {
+    $('.image-frame').hover(function () {
+        $('.image-caption', this).slideToggle('slow');
+    }, function () {
+        $('.image-caption', this).slideToggle('slow');
+    });
+});
+
